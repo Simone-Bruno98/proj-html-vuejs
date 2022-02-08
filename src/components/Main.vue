a1d1885eb83679ff9e5d90dd2bf2c4938c55376f
Simone-Bruno98 <bruno-simone@outlook.it>
2022-02-08 16:51:26 +0100

    Struttura impostata

---
+++ src/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+   
+  </div>
+</template>
+
+<script>
+import whatsVA from './components/Macro/whatsVA.vue'
+import availableServices from './components/Macro/availableServices.vue'
+import sponsors from './components/Macro/sponsors.vue'
+import learnMore from './components/Macro/learnMore.vue'
+
+export default {
+  name: 'Main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
